036aea7201e0f569fe0a04429a48e5e5abe97cbc
Matej Hacin <matej.hacin@gmail.com>
2015-02-17 22:51:25 +0100

    Project uploaded
    Uploaded project files to GitHub.

---
+++ app/src/main/java/com/matejhacin/receivers/AlarmReceiver.java
@@ -0,0 +1,80 @@
+package com.matejhacin.receivers;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+import com.matejhacin.stayhydrated.R;
+
+import java.util.Random;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    AlarmManager alarmManager;
+    PendingIntent alarmIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PendingIntent piDrink = PendingIntent.getBroadcast(context, 0, new Intent(context, DrinkReceiver.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent piLater = PendingIntent.getBroadcast(context, 0, new Intent(context, LaterReceiver.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setTicker("Drinking time!")
+                .setContentTitle(context.getResources().getString(R.string.app_name))
+                .setContentText(getRandomNotificationMessage(context))
+                .addAction(R.drawable.drink_now, "Drink!", piDrink)
+                .addAction(R.drawable.drink_later, "Later!", piLater)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setPriority(NotificationCompat.PRIORITY_MAX);
+        NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        nManager.notify(1, nBuilder.build());
+    }
+
+    public void setAlarm(Context context, int milliseconds)
+    {
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, AlarmReceiver.class), 0);
+
+        // Fire alarm every "milliseconds"
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + milliseconds,
+                milliseconds,
+                alarmIntent);
+
+        // Restart alarm if device is rebooted
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public void cancelAlarm(Context context)
+    {
+        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmIntent = PendingIntent.getBroadcast(context, 0, new Intent(context, AlarmReceiver.class), 0);
+        alarmManager.cancel(alarmIntent);
+
+        // Disable BootReceiver so that alarm won't start again if device is rebooted
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+                PackageManager.DONT_KILL_APP);
+    }
+
+    public String getRandomNotificationMessage(Context context)
+    {
+        String[] messages = context.getResources().getStringArray(R.array.notification_messages);
+        return messages[new Random().nextInt(messages.length)];
+    }
+}
---
+++ app/src/main/java/com/matejhacin/stayhydrated/MainActivity.java
@@ -0,0 +1,126 @@
+package com.matejhacin.stayhydrated;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.matejhacin.receivers.AlarmReceiver;
+
+import info.hoang8f.widget.FButton;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    // Public static variables
+    public static int DEFAULT_POSTPONE_DURATION = 900000; // 15mins
+    public static int DEFAULT_INTERVAL_DURATION = 7200000; // 120mins
+
+    // Views
+    FButton startButton, stopButton;
+
+    // Other
+    SharedPreferences sp;
+
+    // Ad
+    private InterstitialAd ad;
+    private String AD_ID = "ca-app-pub-9931422310281476/5678724146";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create the ad
+        ad = new InterstitialAd(getApplicationContext());
+        ad.setAdUnitId(AD_ID);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        ad.loadAd(adRequest);
+
+        /* SHARED PREFERENCES
+        isRunning - boolean weather notifications are running or not
+        */
+        sp = getSharedPreferences("settings", 0);
+
+        // View variables
+        startButton = (FButton) findViewById(R.id.startButton);
+        stopButton = (FButton) findViewById(R.id.stopButton);
+
+        // Click listeners
+        startButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+
+        // Enable/Disable buttons
+        if (sp.getBoolean("isRunning", false))
+        {
+            disableButton(startButton);
+        }
+        else
+        {
+            disableButton(stopButton);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case (R.id.startButton):
+                new AlarmReceiver().setAlarm(getApplicationContext(), DEFAULT_INTERVAL_DURATION);
+                sp.edit().putBoolean("isRunning", true).apply();
+                disableButton(startButton);
+                enableButton(stopButton);
+                showStartDialog();
+                break;
+            case (R.id.stopButton):
+                new AlarmReceiver().cancelAlarm(getApplicationContext());
+                sp.edit().putBoolean("isRunning", false).apply();
+                disableButton(stopButton);
+                enableButton(startButton);
+                startButton.setEnabled(true);
+                break;
+        }
+    }
+
+    public void showStartDialog()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Done!")
+                .setMessage("When the notification appears, click \"Later!\" to get another notification in 15 minutes.\n\nA one time advertisment will now be shown.")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        displayInterstitial();
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
+    private void disableButton(FButton fBtn)
+    {
+        fBtn.setEnabled(false);
+        fBtn.setButtonColor(getResources().getColor(R.color.gray_disabled));
+        fBtn.setShadowColor(getResources().getColor(R.color.gray_disabled_shadow));
+        fBtn.setTextColor(getResources().getColor(R.color.text_button_disabled));
+    }
+
+    private void enableButton(FButton fBtn)
+    {
+        fBtn.setEnabled(true);
+        fBtn.setButtonColor(getResources().getColor(R.color.blue_enabled));
+        fBtn.setShadowColor(getResources().getColor(R.color.blue_enabled_shadow));
+        fBtn.setTextColor(getResources().getColor(R.color.text_button_enabled));
+    }
+
+    public void displayInterstitial() {
+        if (ad.isLoaded()) {
+            ad.show();
+        }
+    }
+}
